e686fd67d9fb7612265ee7b4c0ddccdd473d4827
bissbotch <qwertsar@gmail.com>
2023-11-16 02:32:30 +0000

    Update

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,17 @@ use std::io::{self, Write};
 use rand::Rng; // Import the rand crate
 
 fn main() {
-    // ... (unchanged main function)
+    // Get command-line arguments
+    let args: Vec<String> = env::args().collect();
+
+    // Check if the correct number of arguments is provided
+    if args.len() != 2 {
+        eprintln!("Usage: {} <file_path>", args[0]);
+        std::process::exit(1);
+    }
+
+    // Extract the file path from the command-line arguments
+    let file_path = &args[1];
 
     match delete_file(file_path) {
         Ok(_) => println!("File securely deleted."),
@@ -23,12 +33,19 @@ fn delete_file(file_path: &str) -> Result<(), io::Error> {
         .write(true)
         .open(file_path)?;
 
-    // Get the file size and determine the number of times to overwrite
+    // Get the file size and calculate the number of times to overwrite
     let file_size = file.metadata()?.len();
     let overwrite_count = (file_size as f64 / buffer_size as f64).ceil() as usize;
 
     for _ in 0..overwrite_count {
         file.write_all(&random_buffer)?;
+    }
+
+    // Write any remaining bytes if the file size is not a multiple of buffer_size
+    let remaining_bytes = (file_size % buffer_size as u64) as usize;
+    if remaining_bytes > 0 {
+        let additional_random_buffer: Vec<u8> = (0..remaining_bytes).map(|_| rng.gen()).collect();
+        file.write_all(&additional_random_buffer)?;
     }
 
     // Delete the file from the file system
